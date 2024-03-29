9694f1b6a304c55b404955f03b2b03966a7ef8f5
Eric R <ericr35811@gmail.com>
2024-03-29 08:31:40 -0400

    Delete js/TimeUpdater.js

--- js/TimeUpdater.js
+++
@@ -1,7 +0,0 @@
-﻿var txtEnteredDateTime = <%  %>
-
-function Updatetime() {
-    txtEnteredDateTime.value = new Date().toLocaleString();
-}
-
-var intervalTime = window.setInterval(Updatetime, 1000);
